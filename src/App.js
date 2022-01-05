15a7820cb9e01ad2a922630915b4d23c1f4b12c3
pk0220 <pratikkhamkar220@gmail.com>
2022-01-05 08:48:48 +0530

    container

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <div>
-      <div style={{color:"black", backgroundColor:"royalblue"}}>Hello</div>
+    <div className="container-fluid">
+      <div className="bg-dark text-light h3 p-3">Good Morning</div>
     </div>
   );
 }
