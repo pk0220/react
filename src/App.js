7c1295ed79ff75dd99ba5749a9dbb1efacf6b30b
pk0220 <pratikkhamkar220@gmail.com>
2022-01-05 08:28:25 +0530

    csss

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import  "./App.css"
+
+
 export default function App() {
   return (
     <div>
-      <h1 className="bg-primary text-light p-2">Hello</h1>
+      <div className="beautify">Hello</div>
     </div>
   );
 }
